613e95e8ca14cbaf3ba1ecd58ef3772ab3bf0916
flyerooo <wxlong@foxmail.com>
2016-03-16 23:36:06 +0800

    编程练习

---
+++ qiwsirPython/ex30.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#coding:utf-8
+#Created by Jeff on 2016/3/16 22:43
+t = eval(raw_input("请输入10个温度（逗号隔开）："))
+print t
+for i in t:
+    print type(i)
---
+++ qiwsirPython/ex31.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#coding:utf-8
+#Created by Jeff on 2016/3/16 22:43
+#28、从键盘输入10个温度（代表10天的气温），计算平均温度，并且显示温度在平均温度之上的天数。
+
+def temperature(t):
+    averageTemperature = sum(t)/10.0
+    return averageTemperature
+def aboveAverageTemperature(t):
+    higherTemperature = 0
+    for i in t:
+        if i > temperature(t):
+            higherTemperature += 1
+    return higherTemperature
+if __name__ == "__main__":
+    while True:
+        t = eval(raw_input("请输入10个温度（逗号隔开）："))
+        if len(t) == 10:
+            break
+        else:
+            print "输入错误"
+    print "平均温度为：",temperature(t)
+    print "平均温度之上的天数为",aboveAverageTemperature(t),"天"
+
